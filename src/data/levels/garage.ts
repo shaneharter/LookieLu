2be703c9efd667bdb1a1835fd1287160ea0a3641
Shane Harter <shane@cronitor.io>
2026-01-08 19:10:37 -0800

    Change name to LookieLu

--- src/data/levels/garage.ts
+++ src/data/levels/garage.ts
@@ -74,7 +74,7 @@ export const garageItems: HiddenItem[] = [
 
 export const garageLevel: Level = {
   id: 'garage',
-  name: 'Dusty Garage',
+  name: 'LookieLu',
   description: 'Find all the hidden items in this cluttered garage!',
   backgroundImage: '/garage-photo.png',
   items: garageItems,
